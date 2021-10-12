a35391406385e917b42878e75919eea827b070ce
Wiktor Karaszewicz <wkaraszewicz@gmail.com>
2021-10-12 15:10:30 +0200

    Test for list view

--- TransportApp/tests/conftest.py
+++ TransportApp/tests/conftest.py
@@ -9,3 +9,31 @@ def cars():
     for car in range(10):
         lst.append(Cars.objects.create(name='car'))
     return lst
+
+
+@pytest.fixture
+def drivers():
+    lst = []
+    for driver in range(10):
+        lst.append(Drivers.objects.create(name='driver'))
+    return lst
+
+
+@pytest.fixture
+def orders():
+    lst = []
+    for order in range(10):
+        lst.append(Orders.objects.create(
+            client="John", phone_number=1231, delivery_address="Warsaw",
+            delivery_time='2012-09-04 06:00:00.000000-08:00'))
+    return lst
+
+
+# @pytest.fixture
+# def transports(orders, drivers, cars):
+#     lst = []
+#     for x in range(10):
+#         lst.append(Transport.objects.create(
+#             car=cars[x], driver=drivers[x], order=orders[x]
+#         ))
+#     return lst
--- TransportApp/tests/tests.py
+++ TransportApp/tests/tests.py
@@ -24,3 +24,36 @@ def test_car_list_is_not_empty(cars):
     assert cars_list.count() == len(cars)
     for car in cars:
         assert car in cars_list
+
+
+@pytest.mark.django_db
+def test_driver_list_is_not_empty(drivers):
+    client = Client()
+    response = client.get(reverse('driver_list_view'))
+    assert response.status_code == 200
+    drivers_list = response.context['object_list']
+    assert drivers_list.count() == len(drivers)
+    for driver in drivers:
+        assert driver in drivers_list
+
+
+@pytest.mark.django_db
+def test_order_list_is_not_empty(orders):
+    client = Client()
+    response = client.get(reverse('order_list_view'))
+    assert response.status_code == 200
+    orders_list = response.context['object_list']
+    assert orders_list.count() == len(orders)
+    for order in orders:
+        assert order in orders_list
+
+
+# @pytest.mark.django_db
+# def test_order_list_is_not_empty(transports):
+#     client = Client()
+#     response = client.get(reverse('transport_list_view'))
+#     assert response.status_code == 200
+#     transports_list = response.context['object_list']
+#     assert transports_list.count() == len(transports)
+#     for transport in transports:
+#         assert transport in transports_list
